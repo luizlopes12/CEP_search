79b50f5c2301ca0526251e27c8641f2c52ec809b
Luiz Lopes <luizlopes12@outlook.com>
2022-03-14 12:03:37 -0300

    formatando documentos

--- src/components/CepForm/index.jsx
+++ src/components/CepForm/index.jsx
@@ -1,45 +1,71 @@
-import React from 'react'
-import { Container, Title } from './styled'
-import TextField from '@mui/material/TextField';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import { useState, useEffect } from 'react'
-import API from '../../services/axios'
+import React from "react";
+import { Container, Title } from "./styled";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import { useState, useEffect } from "react";
+import API from "../../services/axios";
 
 const CepForm = () => {
-    const [locate, setLocate] = useState('')
-    const [cep, setCep] = useState('')
-    useEffect(()=>{
-            API.get(`/${cep}/json/`)
-            .then(async (response) =>{
-                setLocate(response.data)
-            })
-            .catch(async (error)=>{
-                console.log(error)
-            })
-    }, [cep])
-    const handleCep = (e) =>{
-        setCep(e.target.value)
-    }
-    const handleKeyPress = (e) =>{
-        e.key == 'Enter' && setCep(e.target.value)
-    }
+  const [locate, setLocate] = useState("");
+  const [cep, setCep] = useState("");
+  useEffect(() => {
+    API.get(`/${cep}/json/`)
+      .then(async (response) => {
+        setLocate(response.data);
+      })
+      .catch(async (error) => {
+        console.log(error);
+      });
+  }, [cep]);
+  const handleCep = (e) => {
+    setCep(e.target.value);
+  };
+  const handleKeyPress = (e) => {
+    e.key == "Enter" && setCep(e.target.value);
+  };
   return (
     <Container>
-        <Title>CEP Search</Title>
-        <TextField style={{margin: '10px'}} pattern="[0-9]*" size='small' onBlur={handleCep} onKeyDown={handleKeyPress} label='CEP' placeholder='1100100'/>
-        {locate !== '' && (
+      <Title>CEP Search</Title>
+      <TextField
+        style={{ margin: "10px" }}
+        pattern="[0-9]*"
+        size="small"
+        onBlur={handleCep}
+        onKeyDown={handleKeyPress}
+        label="CEP"
+        placeholder="1100100"
+      />
+      {locate !== "" && (
         <>
-            <TextField style={{margin: '10px'}} value={locate.localidade} size='small' label='Cidade' placeholder='Piraporinha do Oeste'/>
-            <TextField style={{margin: '10px'}} value={locate.uf} size='small' label='Estado' placeholder='RJ'/>
-            <Stack direction="row-reverse">
-                <Button style={{margin: '5px'}} variant="contained" color='success' size='large'>Continuar</Button>
-            </Stack>
+          <TextField
+            style={{ margin: "10px" }}
+            value={locate.localidade}
+            size="small"
+            label="Cidade"
+            placeholder="Piraporinha do Oeste"
+          />
+          <TextField
+            style={{ margin: "10px" }}
+            value={locate.uf}
+            size="small"
+            label="Estado"
+            placeholder="RJ"
+          />
+          <Stack direction="row-reverse">
+            <Button
+              style={{ margin: "5px" }}
+              variant="contained"
+              color="success"
+              size="large"
+            >
+              Continuar
+            </Button>
+          </Stack>
         </>
-        )}
-
+      )}
     </Container>
-  )
-}
+  );
+};
 
-export default CepForm
+export default CepForm;

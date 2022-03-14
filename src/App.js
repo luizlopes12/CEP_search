68726c804b4587e18bb5d0c0ac3ed186a27e6ea9
Luiz Lopes <luizlopes12@outlook.com>
2022-03-14 11:39:00 -0300

    Deploy no github pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import CepForm from './components/CepForm'
 import GlobalStyle from './global/GlobalStyles';
+import {BrowserRouter} from 'react-router-dom'
 function App() {
   return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <>
       <GlobalStyle/>
       <CepForm/>
     </>
+    </BrowserRouter>
+
   );
 }
 

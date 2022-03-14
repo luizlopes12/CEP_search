c4107aef50c2b10ae1b17a695227565cc5194446
Luiz Lopes <luizlopes12@outlook.com>
2022-03-14 12:02:53 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import {BrowserRouter} from 'react-router-dom'
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <>
       <GlobalStyle/>
       <CepForm/>
-    </>
     </BrowserRouter>
 
   );
